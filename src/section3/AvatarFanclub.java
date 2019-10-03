c06582535ac7f0a95c4eed2cac1e1c9dc8d83253
RylieM <55446405+RylieM@users.noreply.github.com>
2019-10-02 18:27:00 -0700

    Add files via upload

---
+++ src/section3/AvatarFanclub.java
@@ -0,0 +1,15 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class AvatarFanclub {
+public static void main(String[] args) {
+
+JOptionPane.showInputDialog("Enter your name");
+String name ="The Vsco Girl Lare";
+JOptionPane.showMessageDialog(null,"You entered "+ name);
+
+
+
+}
+}
--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,24 +7,27 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		String name= JOptionPane.showInputDialog("If you find yourself having to cross a \npiranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-
+String lame = JOptionPane.showInputDialog(" Enter adjetive here");
 		// Get the user to enter a type of liquid
-
+String fame= JOptionPane.showInputDialog("enter a type of liquid here");
 		// Get the user to enter a body part
-
+String came= JOptionPane.showInputDialog("Enter a body part here");
 		// Get the user to enter a verb
-
+String bee= JOptionPane.showInputDialog("Enter verb here");
 		// Get the user to enter a place
+String goat= JOptionPane.showInputDialog("Enter a place.");
+
+
 
 		// Fit the user's words into this sentence, and save it in a String:
-		// Piranhas are more [adjective] during the day, so cross the river at
-		// night. Piranhas are attracted to fresh [type of liquid] and will most
-		// likely take a bite out of your [body part] if you [verb]. Whatever
-		// you do, if you have an open wound, try to find another way to get
-		// back to the [place]. Good luck!
 		
+		JOptionPane.showMessageDialog(null," Piranhas are more "+lame+" during the day, so cross the river at\n" + 
+				"		 night. Piranhas are attracted to fresh "+fame+" and will most\n" + 
+				"		 likely take a bite out of your "+came+" if you "+bee+". Whatever\n" + 
+				"		 you do, if you have an open wound, try to find another way to get\n" + 
+				"		 back to the "+goat+". Good luck!");
 		// Make a pop-up for the final story. You can use \n to go to the next line
 		
 
